ee939f4b6c1be52c93571370415fc298a2982d3a
rafaelpolo19 <47400788+rafaelpolo19@users.noreply.github.com>
2019-03-14 09:30:40 -0700

    dd

--- run.sh
+++
@@ -1,3 +0,0 @@
-#Comando para arrancar el contenedor
-#se debe estar parado en la ruta especifica al proyecto de katalon.
-docker run -t -v "$(pwd)":/katalon/katalon/source katalonpr katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/Zoho"
